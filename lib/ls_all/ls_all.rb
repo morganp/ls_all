0a9ce3611fa7b75e554bb318e0680118f615e81f
Morgan Prior <morgan@amaras-tech.co.uk>
2010-10-03 15:12:01 +0100

    Various refactors

--- lib/ls_all/ls_all.rb
+++ lib/ls_all/ls_all.rb
@@ -1,47 +1,62 @@
 #!/usr/bin/env ruby
 
-require 'rubygems'
-require 'ls_all_opts'
+module LsAll
+   class LsAll
 
-class LsAll
-
-   def initialize( max_depth=0 )
-      @max_depth = max_depth
-   end
-
-   def list_files( files, spacer='', path='.', depth=1)
-      path = File.expand_path( path )
-
-      files.each do |f|
-         if File.directory?( f )
-
-            puts spacer + f + '/'   
-
-            recursive_path=File.expand_path( f )
-            Dir.chdir( recursive_path )
-            recursive_files = Dir.glob('*')
-            recursive_spacer = spacer + ' '
-
-            if (@max_dpeth == 0) or (depth < @max_depth)
-               rec_depth=depth+1
-               list_files(recursive_files, recursive_spacer, recursive_path, rec_depth)
-            else 
-               #puts spacer + f
-            end
-         else
-            puts spacer + f
-         end
-         Dir.chdir( path )
+      def initialize( max_depth=0, count=false )
+         @max_depth = max_depth
+         @count     = count
       end
 
+      def list_files( files, spacer='', path='.', depth=1)
+         path = File.expand_path( path )
+
+         files.each do |f|
+            if File.directory?( f )
+               
+               file_string= f + '/'
+               if  (not @max_depth == 0) and (depth == @max_depth) and (@count == true)
+                  puts spacer + file_string.ljust(20) + ' Count: ' + file_count( f )
+               else
+                  puts spacer + file_string   
+               end
+
+
+               recursive_path=File.expand_path( f )
+               Dir.chdir( recursive_path )
+               recursive_files = Dir.glob('*')
+               recursive_spacer = spacer + ' '
+
+               if (@max_depth == 0) or (depth < @max_depth)
+                  rec_depth=depth+1 
+                  list_files(recursive_files, recursive_spacer, recursive_path, rec_depth)
+               else
+                  #
+               end
+            else
+               puts spacer + f
+            end
+            Dir.chdir( path )
+         end
+
+      end
+
+      def file_count( path )
+         recursive_files = Dir.glob(path + '/*')
+         recursive_files.size.to_s
+      end
    end
 end
 
 if $0 == __FILE__
-   options = LsAllOpts.parse(ARGV)
-   list    = LsAll.new( options.depth )
 
-   #Dir.chdir( path ) 
-   files = Dir.glob('*')
+   require File.join(File.dirname(__FILE__), "ls_all_opts")
+
+   options = LsAll::LsAllOpts.parse(ARGV)
+   list    = LsAll::LsAll.new( options.depth, options.count )
+
+   files = Dir.glob(options.path + '/*')
    list.list_files( files )
 end
+
+
